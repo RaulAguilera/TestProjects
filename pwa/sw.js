809f8c34e5a3b5a4c167721ada0ad439ac497652
raul.aguilera <jrat9fra@gmail.com>
2019-03-25 17:19:38 -0600

    Added first example of PWA

---
+++ pwa/sw.js
@@ -0,0 +1,33 @@
+const cacheName = 'cache-v1';
+const resourcesToPrecache = [
+    '/',
+    'index.html',
+    'styles/main.css',
+    'images/entangles.jpg',
+    'images/quantumentanglement.jpg'
+]
+
+self.addEventListener('install', event => {
+    console.log("Install event!");
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(resourcesToPrecache);
+            })
+
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log("Activate event!");
+});
+
+self.addEventListener('fetch', event => {
+    console.log("Fetch intercepted for: ", event.request.url);
+
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
