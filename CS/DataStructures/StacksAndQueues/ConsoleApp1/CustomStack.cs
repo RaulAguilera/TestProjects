21d61975272058cdd29be3b6aba606af879f210f
raul.aguilera <jrat9fra@gmail.com>
2019-07-05 15:10:26 -0500

    Added stack implementation

---
+++ CS/DataStructures/StacksAndQueues/ConsoleApp1/CustomStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    public class CustomStack
+    {
+        public int[] Stack { get; set; }
+        private int Top { get; set; } = -1;
+
+        public CustomStack(int size) {
+            Stack = new int[size];
+        }
+
+        private bool IsStackEmpty() {
+            return Top == -1;
+        }
+
+        public void Push(int item) {
+            Top += 1;
+            Stack[Top] = item;
+        }
+
+        public int Pop()
+        {
+            if (IsStackEmpty())
+            {
+                return -1;
+            }
+
+            int item = Stack[Top];
+            Top -= 1;
+            return item;
+        }
+    }
+}
---
+++ CS/DataStructures/StacksAndQueues/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customStack = new CustomStack(5);
+
+            customStack.Push(5);
+            customStack.Push(7);
+            customStack.Push(9);
+
+            Console.WriteLine(customStack.Pop());
+            Console.WriteLine(customStack.Pop());
+            Console.WriteLine(customStack.Pop());
+            Console.WriteLine(customStack.Pop());
+
+
+            Console.ReadKey();
+        }
+    }
+}
